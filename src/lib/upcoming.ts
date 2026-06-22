24d030e174e92b54e3aa8d3b1e3876d9b804b00f
Christian Legge <christian@christianlegge.dev>
2026-06-22 16:21:24 -0400

    add upcoming

---
+++ src/lib/upcoming.ts
@@ -0,0 +1,238 @@
+import type { LetterColor } from './colorize';
+
+const upcoming: { pattern: LetterColor[][]; target: string }[] = [
+	{
+		pattern: [
+			['green', 'green', 'green', 'black', 'black'],
+			['black', 'black', 'yellow', 'yellow', 'yellow'],
+			['green', 'green', 'green', 'black', 'black'],
+			['black', 'black', 'yellow', 'yellow', 'yellow'],
+			['green', 'green', 'green', 'black', 'black'],
+			['black', 'black', 'yellow', 'yellow', 'yellow']
+		],
+		target: 'black'
+	},
+	{
+		pattern: [
+			['black', 'black', 'green', 'green', 'green'],
+			['black', 'black', 'green', 'yellow', 'green'],
+			['black', 'black', 'green', 'green', 'green'],
+			['yellow', 'yellow', 'yellow', 'black', 'black'],
+			['yellow', 'green', 'yellow', 'black', 'black'],
+			['yellow', 'yellow', 'yellow', 'black', 'black']
+		],
+		target: 'elite'
+	},
+	{
+		pattern: [
+			['yellow', 'yellow', 'yellow', 'yellow', 'yellow'],
+			['yellow', 'black', 'yellow', 'black', 'yellow'],
+			['yellow', 'yellow', 'yellow', 'yellow', 'yellow'],
+			['black', 'yellow', 'yellow', 'yellow', 'black'],
+			['yellow', 'black', 'black', 'black', 'yellow'],
+			['yellow', 'yellow', 'yellow', 'yellow', 'yellow']
+		],
+		target: 'spear'
+	},
+	{
+		pattern: [
+			['green', 'yellow', 'black', 'black', 'black'],
+			['yellow', 'green', 'yellow', 'black', 'black'],
+			['black', 'yellow', 'green', 'green', 'black'],
+			['black', 'green', 'green', 'yellow', 'black'],
+			['black', 'black', 'yellow', 'green', 'yellow'],
+			['black', 'black', 'black', 'yellow', 'green']
+		],
+		target: 'media'
+	},
+	{
+		pattern: [
+			['black', 'black', 'green', 'green', 'green'],
+			['black', 'black', 'green', 'yellow', 'yellow'],
+			['black', 'black', 'green', 'yellow', 'black'],
+			['black', 'yellow', 'green', 'black', 'black'],
+			['yellow', 'yellow', 'green', 'black', 'black'],
+			['green', 'green', 'green', 'black', 'black']
+		],
+		target: 'crank'
+	},
+	{
+		pattern: [
+			['black', 'black', 'yellow', 'yellow', 'green'],
+			['yellow', 'yellow', 'yellow', 'black', 'black'],
+			['black', 'black', 'yellow', 'green', 'green'],
+			['green', 'green', 'yellow', 'black', 'black'],
+			['black', 'black', 'yellow', 'yellow', 'yellow'],
+			['green', 'yellow', 'yellow', 'black', 'black']
+		],
+		target: 'terse'
+	},
+	{
+		pattern: [
+			['yellow', 'yellow', 'black', 'black', 'black'],
+			['green', 'yellow', 'yellow', 'black', 'black'],
+			['green', 'green', 'yellow', 'yellow', 'black'],
+			['black', 'green', 'green', 'yellow', 'yellow'],
+			['black', 'black', 'green', 'green', 'yellow'],
+			['black', 'black', 'black', 'green', 'green']
+		],
+		target: 'spear'
+	},
+	{
+		pattern: [
+			['black', 'black', 'yellow', 'black', 'black'],
+			['black', 'yellow', 'green', 'yellow', 'black'],
+			['yellow', 'green', 'black', 'green', 'yellow'],
+			['green', 'yellow', 'black', 'yellow', 'green'],
+			['black', 'green', 'yellow', 'green', 'black'],
+			['black', 'black', 'green', 'black', 'black']
+		],
+		target: 'ripen'
+	},
+	{
+		pattern: [
+			['yellow', 'yellow', 'black', 'yellow', 'yellow'],
+			['yellow', 'black', 'black', 'black', 'yellow'],
+			['black', 'green', 'black', 'green', 'black'],
+			['black', 'yellow', 'black', 'yellow', 'black'],
+			['green', 'black', 'black', 'black', 'green'],
+			['green', 'green', 'black', 'green', 'green']
+		],
+		target: 'caper'
+	},
+	{
+		pattern: [
+			['yellow', 'yellow', 'black', 'yellow', 'yellow'],
+			['yellow', 'black', 'black', 'black', 'yellow'],
+			['black', 'green', 'black', 'green', 'black'],
+			['black', 'yellow', 'black', 'yellow', 'black'],
+			['green', 'black', 'black', 'black', 'green'],
+			['green', 'green', 'black', 'green', 'green']
+		],
+		target: 'champ'
+	},
+	{
+		pattern: [
+			['black', 'green', 'yellow', 'green', 'black'],
+			['black', 'green', 'green', 'green', 'black'],
+			['black', 'green', 'yellow', 'green', 'black'],
+			['black', 'yellow', 'green', 'yellow', 'black'],
+			['black', 'yellow', 'yellow', 'yellow', 'black'],
+			['black', 'yellow', 'green', 'yellow', 'black']
+		],
+		target: 'brood'
+	},
+	{
+		pattern: [
+			['black', 'green', 'green', 'green', 'black'],
+			['green', 'black', 'yellow', 'black', 'green'],
+			['black', 'yellow', 'green', 'yellow', 'black'],
+			['black', 'green', 'yellow', 'green', 'black'],
+			['yellow', 'black', 'green', 'black', 'yellow'],
+			['black', 'yellow', 'yellow', 'yellow', 'black']
+		],
+		target: 'bloat'
+	},
+	{
+		pattern: [
+			['green', 'black', 'green', 'black', 'green'],
+			['black', 'green', 'black', 'green', 'black'],
+			['black', 'black', 'green', 'black', 'black'],
+			['black', 'black', 'yellow', 'black', 'black'],
+			['black', 'yellow', 'black', 'yellow', 'black'],
+			['yellow', 'black', 'yellow', 'black', 'yellow']
+		],
+		target: 'elect'
+	},
+	{
+		pattern: [
+			['yellow', 'black', 'green', 'black', 'yellow'],
+			['black', 'yellow', 'green', 'yellow', 'black'],
+			['black', 'green', 'yellow', 'green', 'black'],
+			['black', 'yellow', 'green', 'yellow', 'black'],
+			['black', 'green', 'yellow', 'green', 'black'],
+			['green', 'black', 'yellow', 'black', 'green']
+		],
+		target: 'fetal'
+	},
+	{
+		pattern: [
+			['green', 'black', 'yellow', 'black', 'green'],
+			['green', 'black', 'yellow', 'black', 'green'],
+			['green', 'black', 'yellow', 'black', 'green'],
+			['black', 'green', 'yellow', 'green', 'black'],
+			['black', 'green', 'yellow', 'green', 'black'],
+			['black', 'green', 'yellow', 'green', 'black']
+		],
+		target: 'title'
+	},
+	{
+		pattern: [
+			['black', 'black', 'black', 'black', 'yellow'],
+			['black', 'black', 'black', 'yellow', 'green'],
+			['black', 'black', 'yellow', 'green', 'green'],
+			['black', 'yellow', 'green', 'green', 'yellow'],
+			['yellow', 'green', 'green', 'yellow', 'black'],
+			['green', 'green', 'yellow', 'black', 'black']
+		],
+		target: 'plied'
+	},
+	{
+		pattern: [
+			['green', 'yellow', 'black', 'black', 'black'],
+			['black', 'green', 'yellow', 'black', 'black'],
+			['black', 'black', 'green', 'yellow', 'black'],
+			['black', 'black', 'green', 'yellow', 'black'],
+			['black', 'green', 'yellow', 'black', 'black'],
+			['green', 'yellow', 'black', 'black', 'black']
+		],
+		target: 'mouse'
+	},
+	{
+		pattern: [
+			['black', 'yellow', 'green', 'black', 'black'],
+			['black', 'black', 'yellow', 'green', 'black'],
+			['black', 'black', 'black', 'yellow', 'green'],
+			['black', 'black', 'black', 'yellow', 'green'],
+			['black', 'black', 'yellow', 'green', 'black'],
+			['black', 'yellow', 'green', 'black', 'black']
+		],
+		target: 'covey'
+	},
+	{
+		pattern: [
+			['yellow', 'green', 'black', 'black', 'black'],
+			['black', 'yellow', 'green', 'black', 'black'],
+			['black', 'black', 'yellow', 'green', 'black'],
+			['black', 'black', 'yellow', 'green', 'black'],
+			['black', 'yellow', 'green', 'black', 'black'],
+			['yellow', 'green', 'black', 'black', 'black']
+		],
+		target: 'cider'
+	},
+	{
+		pattern: [
+			['yellow', 'green', 'black', 'black', 'black'],
+			['black', 'yellow', 'green', 'black', 'black'],
+			['black', 'black', 'yellow', 'green', 'black'],
+			['black', 'black', 'yellow', 'green', 'black'],
+			['black', 'yellow', 'green', 'black', 'black'],
+			['yellow', 'green', 'black', 'black', 'black']
+		],
+		target: 'cider'
+	},
+	{
+		pattern: [
+			['yellow', 'green', 'yellow', 'black', 'black'],
+			['black', 'yellow', 'green', 'yellow', 'black'],
+			['black', 'black', 'yellow', 'green', 'yellow'],
+			['black', 'black', 'yellow', 'green', 'yellow'],
+			['black', 'yellow', 'green', 'yellow', 'black'],
+			['yellow', 'green', 'yellow', 'black', 'black']
+		],
+		target: 'board'
+	}
+];
+
+export default upcoming;
+
---
+++ src/routes/upcoming/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import upcoming from '$lib/upcoming';
+	import Pattern from '../../components/Pattern.svelte';
+	import { SvelteDate } from 'svelte/reactivity';
+
+	const today = new SvelteDate();
+
+	function offsetDate(days: number) {
+		const d = new SvelteDate(today);
+		d.setDate(d.getDate() + days);
+		return d;
+	}
+</script>
+
+<div class="flex w-full flex-row flex-wrap gap-8">
+	{#each upcoming as { pattern, target }, i (i)}
+		<div class="flex flex-col items-center justify-center gap-2">
+			<span>{offsetDate(i).toDateString()}</span>
+			<span>{target}</span>
+			<Pattern {pattern} />
+		</div>
+	{/each}
+</div>

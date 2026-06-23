4a193212b5833d6ada1f8e8c2b57f74e15ff597e
Christian Legge <christian@christianlegge.dev>
2026-06-23 00:15:54 -0400

    favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+	<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+	<link rel="shortcut icon" href="/favicon.ico" />
+	<meta name="apple-mobile-web-app-title" content="zipdle" />
+	<link rel="manifest" href="/site.webmanifest" />
+</svelte:head>
 {@render children()}

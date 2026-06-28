63b837fcf5b52ac6982e63e32f7473abd38dc36e
Christian Legge <christian@christianlegge.dev>
2026-06-28 16:16:57 -0400

    archive page

--- src/components/Month.svelte
+++ src/components/Month.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { resolve } from '$app/paths';
+
 	let { monthIndex, year }: { monthIndex: number; year: number } = $props();
 	const months = [
 		'January',
@@ -18,16 +20,43 @@
 	const days = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'];
 
 	const startDay = $derived(new Date(year, monthIndex, 1).getDay());
+
+	function hasPuzzle(day: number) {
+		if (year < 2026) {
+			return false;
+		}
+		if (year === 2026 && monthIndex < 5) {
+			return false;
+		}
+		if (year === 2026 && monthIndex === 5 && day < 23) {
+			return false;
+		}
+		if (new Date(year, monthIndex, day) > new Date()) {
+			return false;
+		}
+		return true;
+	}
 </script>
 
-<section>
+<section class="flex flex-col items-center">
 	<h2>{months[monthIndex]} {year}</h2>
-	<div class="grid grid-cols-7">
+	<div class="grid grid-cols-7 gap-1">
 		{#each days as day (day)}
-			<span>{day}</span>
+			<span class="text-center">{day}</span>
 		{/each}
 		{#each [...new Array(startDay).fill(null), ...new Array(30).fill(true)] as v, i (i)}
-			<div>{v ? i + 1 - startDay : ''}</div>
+			{#if v && !hasPuzzle(i + 1 - startDay)}
+				<div class="bg-slate-600 p-1 text-center">{v ? i + 1 - startDay : ''}</div>
+			{:else}
+				<a
+					href={resolve(
+						`/${year}-${(monthIndex + 1).toString().padStart(2, '0')}-${(i + 1 - startDay)
+							.toString()
+							.padStart(2, '0')}`
+					)}
+					class="{v ? 'bg-slate-400' : ''} p-1 text-center">{v ? i + 1 - startDay : ''}</a
+				>
+			{/if}
 		{/each}
 	</div>
 </section>
--- src/routes/archive/+page.svelte
+++ src/routes/archive/+page.svelte
@@ -1,5 +1,29 @@
 <script lang="ts">
 	import Month from '../../components/Month.svelte';
+
+	const START_DATE = new Date(2026, 5, 23);
+
+	const today = new Date();
+
+	const allMonths: { year: number; month: number }[] = [];
+
+	let month = START_DATE.getMonth();
+	let year = START_DATE.getFullYear();
+	while (year !== today.getFullYear() || month !== today.getMonth()) {
+		allMonths.push({ year, month });
+		month++;
+		if (month >= 12) {
+			month = 0;
+			year++;
+		}
+	}
+	allMonths.push({ year, month });
 </script>
 
-<Month monthIndex={5} year={2026} />
+<div
+	class="col-span-2 mx-auto flex max-w-300 flex-row flex-wrap items-start justify-center gap-10 px-4"
+>
+	{#each allMonths as { year, month } (`${year}${month}`)}
+		<Month monthIndex={month} {year} />
+	{/each}
+</div>
